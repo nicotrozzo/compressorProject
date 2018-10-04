d09ac899c7ffe6c37e0f0817215c8acf121a52df
nicotrozzo <37149263+nicotrozzo@users.noreply.github.com>
2018-10-04 13:51:00 -0300

    Update decompressor.cpp

---
+++ compressorProject/compressorProject/decompressor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "lodepng.h"
+ 
+using namespace std;
+
+bool decompress(const char* filename)
+{
+  string pngName = filename;
+  pngName. 
+  LodePNG_encode32_file(pngName, rgb,width, height);
+
+
+}
